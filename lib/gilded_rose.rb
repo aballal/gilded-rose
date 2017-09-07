2fd76593cb7abd15981159b4ded39359168d8935
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-09-07 18:57:27 +0100

    Refactored sell_in < 0

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,6 +8,7 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+
       unless EXCEPTIONS.include?(item.name)
         item.quality = [item.quality - 1, 0].max
       else
@@ -17,26 +18,21 @@ class GildedRose
           item.quality = [item.quality + 1, 50].min if item.sell_in < 6
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+
+      item.sell_in = item.sell_in - 1 if item.name != "Sulfuras, Hand of Ragnaros"
+
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
+        unless EXCEPTIONS.include?(item.name)
+          if item.quality > 0
+            item.quality = item.quality - 1
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          item.quality = [item.quality + 1, 50].min if item.name == "Aged Brie"
         end
       end
+
     end
+
   end
 end
