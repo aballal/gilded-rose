f3b20fd5641fd49123810a6f9fd648f51cfb6f62
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-09-07 21:34:07 +0100

    Refactored items other than backstage passes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,24 +9,18 @@ class GildedRose
     @items.each do |item|
       next if item.name == "Sulfuras, Hand of Ragnaros"
 
-      change = EXCEPTIONS.include?(item.name) ? 1 : -1
-      item.quality = [[item.quality + change, 0].max , 50].min
-
       if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        item.quality = [item.quality + 1, 50].min
         item.quality = [item.quality + 1, 50].min if item.sell_in < 11
         item.quality = [item.quality + 1, 50].min if item.sell_in < 6
+        item.quality = 0 if item.sell_in <= 0
+        next
       end
 
-      item.sell_in = item.sell_in - 1
-
-      if item.sell_in < 0
-        if EXCEPTIONS.include?(item.name)
-          item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          item.quality = [item.quality + 1, 50].min if item.name == "Aged Brie"
-        else
-          item.quality = [item.quality - 1, 0].max
-        end
-      end
+      change = EXCEPTIONS.include?(item.name) ? 1 : -1
+      item.sell_in -= 1
+      change *= 2 if item.sell_in < 0
+      item.quality = [[item.quality + change, 0].max , 50].min
     end
   end
 end
