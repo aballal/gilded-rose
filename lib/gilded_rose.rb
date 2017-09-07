9801d2c440e2e3d1c31edf8dc7a275fe4a64ea41
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-09-07 22:57:31 +0100

    Refactored passes. Added comments

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,20 +7,27 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      next if item.name == "Sulfuras, Hand of Ragnaros"
+      next if item.name == "Sulfuras, Hand of Ragnaros" # No change to sulfuras
 
       if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        item.quality = [item.quality + 1, 50].min
-        item.quality = [item.quality + 1, 50].min if item.sell_in < 11
-        item.quality = [item.quality + 1, 50].min if item.sell_in < 6
-        item.quality = 0 if item.sell_in <= 0
+        item.quality = [item.quality + change_for_passes(item), 50].min
         next
       end
 
-      change = EXCEPTIONS.include?(item.name) ? 1 : -1
       item.sell_in -= 1
-      change *= 2 if item.sell_in < 0
-      item.quality = [[item.quality + change, 0].max , 50].min
+
+      change = EXCEPTIONS.include?(item.name) ? 1 : -1 # Exceptions increase with age, others decrease
+      change *= 2 if item.sell_in < 0 # After the sell by date the change is twice as much
+      item.quality = [[item.quality + change, 0].max , 50].min # Quantity is a minimum of 0 and maximum of 50
     end
+  end
+
+  private
+
+  def change_for_passes(item)
+    return -item.quality if item.sell_in <= 0
+    return 3 if item.sell_in < 6
+    return 2 if item.sell_in < 11
+    1
   end
 end
