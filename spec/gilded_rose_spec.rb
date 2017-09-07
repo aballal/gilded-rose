d15ff2c5daf7abf91ada7ba795538e6f87c066dc
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-09-07 17:16:58 +0100

    Added tests for Aged Brie passing the sell by date. Added tests for items that are not a special case.

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,21 +2,16 @@ require 'gilded_rose'
 require 'item'
 
 describe GildedRose do
-  # Post refactoring replace real instances with doubles
-  # Also change expectations to see Item's method has been invoked rather than checking for Item's attribute
-
-  # let(:vest) { double(:item, :name => '+5 Dexterity Vest', :sell_in => 10, :quality => 20) }
-  # let(:brie) { double(:item, :name => 'Aged Brie', :sell_in => 2, :quality => 0) }
-  # let(:elixir) { double(:item, :name => 'Elixir of the Mongoose', :sell_in => 5, :quality => 7) }
-  # let(:sulfuras) { double(:item, :name => 'Sulfuras, Hand of Ragnaros', :sell_in => 0, :quality => 80) }
-  # let(:passes) { double(:item, :name => 'Backstage passes to a TAFKAL80ETC concert', :sell_in => 15, :quality => 20) }
-
   let(:vest) { Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20) }
+  let(:vest_q) { Item.new(name="+5 Dexterity Vest", sell_in=10, quality=0) }
 
   let(:brie) { Item.new(name="Aged Brie", sell_in=2, quality=0) }
   let(:brie_q) { Item.new(name="Aged Brie", sell_in=2, quality=50) }
+  let(:brie2) { Item.new(name="Aged Brie", sell_in=0, quality=0) }
+  let(:brie2_q) { Item.new(name="Aged Brie", sell_in=0, quality=49) }
 
-  let(:elixir) { Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7) }
+  let(:elixir) { Item.new(name="Elixir of the Mongoose", sell_in=0, quality=7) }
+  let(:elixir_q) { Item.new(name="Elixir of the Mongoose", sell_in=0, quality=1) }
 
   let(:sulfuras) { Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80) }
 
@@ -28,8 +23,8 @@ describe GildedRose do
   let(:passes4) { Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20) }
   let(:passes4_q) { Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=50) }
 
-  let(:items) { [vest, brie, brie_q, elixir, sulfuras, passes, passes2, passes2_q,
-    passes3, passes3_q, passes4, passes4_q] }
+  let(:items) { [vest, vest_q, brie, brie_q, brie2, brie2_q, elixir, elixir_q, sulfuras,
+    passes, passes2, passes2_q, passes3, passes3_q, passes4, passes4_q] }
 
   subject(:gilded_rose) { described_class.new(items) }
 
@@ -39,12 +34,24 @@ describe GildedRose do
     end
 
     context 'item is Aged Brie' do
-      it 'increases the quality by 1' do
-        expect(brie.quality).to eq 1
+      context 'sell by date has not passed' do
+        it 'increases the quality by 1' do
+          expect(brie.quality).to eq 1
+        end
+
+        it 'does not increase the quality beyond 50' do
+          expect(brie_q.quality).to eq 50
+        end
       end
 
-      it 'does not increase the quality beyond 50' do
-        expect(brie_q.quality).to eq 50
+      context 'sell by date has passed' do
+        it 'increases the quality by 1' do
+          expect(brie2.quality).to eq 2
+        end
+
+        it 'does not increase the quality beyond 50' do
+          expect(brie2_q.quality).to eq 50
+        end
       end
     end
 
@@ -82,6 +89,34 @@ describe GildedRose do
 
         it 'does not increase the quality beyond 50' do
           expect(passes4_q.quality).to eq 50
+        end
+      end
+    end
+
+    context 'item is Sulfuras, Hand of Ragnaros' do
+      it 'does not change in quality' do
+        expect(sulfuras.quality).to eq 80
+      end
+    end
+
+    context 'item is any of the others i.e. besides brie, passes and sulfuras' do
+      context 'sell by date has not passed' do
+        it 'reduces the quality by 1' do
+          expect(vest.quality).to eq 19
+        end
+
+        it 'does not reduce the quality beyond 0' do
+          expect(vest_q.quality).to eq 0
+        end
+      end
+
+      context 'sell by date has passed' do
+        it 'reduces the quality by 2' do
+          expect(elixir.quality).to eq 5
+        end
+
+        it 'does not reduce the quality beyond 0' do
+          expect(elixir_q.quality).to eq 0
         end
       end
     end
