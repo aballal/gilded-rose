d4f1c8c9824790c71ca38f7b974a757b976bccf6
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-09-07 21:17:44 +0100

    Removed repeated checks for Sulfuras using next. Removed branching for exception by setting change as positive or negative

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,24 +1,23 @@
 class GildedRose
   EXCEPTIONS = ["Aged Brie",
-                "Backstage passes to a TAFKAL80ETC concert",
-                "Sulfuras, Hand of Ragnaros"]
+                "Backstage passes to a TAFKAL80ETC concert"]
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if EXCEPTIONS.include?(item.name)
-        item.quality = [item.quality + 1, 50].min unless item.name == "Sulfuras, Hand of Ragnaros"
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          item.quality = [item.quality + 1, 50].min if item.sell_in < 11
-          item.quality = [item.quality + 1, 50].min if item.sell_in < 6
-        end
-      else
-        item.quality = [item.quality - 1, 0].max
+      next if item.name == "Sulfuras, Hand of Ragnaros"
+
+      change = EXCEPTIONS.include?(item.name) ? 1 : -1
+      item.quality = [[item.quality + change, 0].max , 50].min
+
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        item.quality = [item.quality + 1, 50].min if item.sell_in < 11
+        item.quality = [item.quality + 1, 50].min if item.sell_in < 6
       end
 
-      item.sell_in = item.sell_in - 1 if item.name != "Sulfuras, Hand of Ragnaros"
+      item.sell_in = item.sell_in - 1
 
       if item.sell_in < 0
         if EXCEPTIONS.include?(item.name)
@@ -28,8 +27,6 @@ class GildedRose
           item.quality = [item.quality - 1, 0].max
         end
       end
-
     end
-
   end
 end
